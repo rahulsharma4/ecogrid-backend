c75048093586ad938b2c187d9beeb74a59dde6e5
Rahul <rahulbhardwaz2k1@gmail.com>
2026-05-27 16:50:10 +0530

    changes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { authUser, registerUser } = require('../controllers/userController');
+const { authUser } = require('../controllers/userController');
 
 router.post('/login', authUser);
-router.post('/register', registerUser);
+router.post('/register', (req, res) => {
+  res.status(403).json({ message: 'Public registration is disabled. Please contact the administrator.' });
+});
 
 module.exports = router;
